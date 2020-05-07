836007bd63e693e38e93f00a6bf4447fa3bcc48a
Joseph Yu <joseph.k.yu12@gmail.com>
2020-05-07 02:24:40 -0400

    deleted extraneous reference code

--- LearningDynamicsPNAS/SOD_Utils/arclength_mod.m
+++ LearningDynamicsPNAS/SOD_Utils/arclength_mod.m
@@ -37,36 +37,5 @@ function a = arclength_mod(p, q)
     elseif d == 2 % 2D case in R3
         
     end
-    
-    %%
-
-    if nargin==1
-        qn = pn;
-    else
-        [d, qn] = size(q);
-    end
-
-    if pn == 0 || qn == 0
-        m = zeros(pn,qn);
-        return
-    end
-
-    if nargin == 1
-        pmag = col_sum(p .* p);
-        qmag = pmag;
-        m = abs(repmat(qmag, pn, 1) + repmat(pmag', 1, qn) - 2*p'*p); 
-        m(1:qn+1:end) = 0;
-    elseif nargin == 2    
-        pmag = col_sum(p .* p);
-        qmag = col_sum(q .* q);
-        m = abs(repmat(qmag, pn, 1) + repmat(pmag', 1, qn) - 2*p'*q);
-        %m = ones(pn,1)*qmag + pmag'*ones(1,qn) - 2*p'*q;    
-    else    
-        Ap = A*p;
-        Aq = A*q;
-        pmag = col_sum(p .* Ap);
-        qmag = col_sum(q .* Aq);
-        m = abs(repmat(qmag, pn, 1) + repmat(pmag', 1, qn) - 2*p'*Aq);   
-    end
 
 return
