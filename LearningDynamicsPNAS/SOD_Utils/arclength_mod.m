a2437812c9468b40bf2730dd82a56c7575bfc996
Joseph Yu <joseph.k.yu12@gmail.com>
2020-05-07 01:29:33 -0400

    added and integrated arclength_mod.m into partition_traj.m

--- LearningDynamicsPNAS/SOD_Utils/arclength_mod.m
+++ LearningDynamicsPNAS/SOD_Utils/arclength_mod.m
@@ -1,19 +1,72 @@
-function a = arclength_mod(p)
+function a = arclength_mod(p, q)
     % takes in geodesic coordinates and returns a matrix of pairwise
     % distances
     
-    d = size(p,1); % dimension of coordinates (either 1 or 2)
-    n = size(p,2); % number of pairwise points
-    a = NaN(n);    % initializing pairwise distance matrix
+    [d, pn] = size(p); % d = dimension of coordinates (either 1 or 2); pn is the number of pairwise points
     
-    if d == 1
-        for i = 1:n
-           for j = 1:n
-               a(i,j) = min( abs(p(i) - p(j)) , 2*pi - abs(p(i) - p(j)) );
-           end
+    % determining whether only one argument is passed in or not
+    if nargin==1
+        qn = pn;
+    else
+        [d, qn] = size(q);
+    end
+    % if there is nothing (pn = qn = 0), then we return an empty matrix
+    if pn == 0 || qn == 0
+        a = zeros(pn,qn); % an empty matrix
+        return
+    end
+    
+    a = NaN(pn, qn);    % initializing pairwise distance matrix
+    
+    if d == 1 % 1D case (circle) in R2
+        if nargin == 1
+            for i = 1:pn
+               for j = 1:pn
+                   a(i,j) = min( abs(p(i) - p(j)) , 2*pi - abs(p(i) - p(j)) );
+               end
+            end
+        end
+        if nargin == 2
+            for i = 1:pn
+               for j = 1:qn
+                   a(i,j) = min( abs(p(i) - q(j)) , 2*pi - abs(p(i) - q(j)) );
+               end
+            end
         end
         
-    elseif d == 2
+    elseif d == 2 % 2D case in R3
         
     end
+    
+    %%
+
+    if nargin==1
+        qn = pn;
+    else
+        [d, qn] = size(q);
+    end
+
+    if pn == 0 || qn == 0
+        m = zeros(pn,qn);
+        return
+    end
+
+    if nargin == 1
+        pmag = col_sum(p .* p);
+        qmag = pmag;
+        m = abs(repmat(qmag, pn, 1) + repmat(pmag', 1, qn) - 2*p'*p); 
+        m(1:qn+1:end) = 0;
+    elseif nargin == 2    
+        pmag = col_sum(p .* p);
+        qmag = col_sum(q .* q);
+        m = abs(repmat(qmag, pn, 1) + repmat(pmag', 1, qn) - 2*p'*q);
+        %m = ones(pn,1)*qmag + pmag'*ones(1,qn) - 2*p'*q;    
+    else    
+        Ap = A*p;
+        Aq = A*q;
+        pmag = col_sum(p .* Ap);
+        qmag = col_sum(q .* Aq);
+        m = abs(repmat(qmag, pn, 1) + repmat(pmag', 1, qn) - 2*p'*Aq);   
+    end
+
 return
